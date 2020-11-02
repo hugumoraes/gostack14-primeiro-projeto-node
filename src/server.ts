069355da01277ef641c5c47433dbcda41ef5a72d
Hugo <hugomoraes_@hotmail.com>
2020-11-02 20:27:16 -0300

    Inicializando módulo

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import routes from './routes';
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  const { name, email } = req.body;
+
+  return res.json({ message: 'Hello world' });
+});
+
+app.listen(3333, () => {
+  console.log('Server started on port 3333!');
+});
